f467837ccce02cbc9de7b05127b08e3c0e831652
Wilco Louwerse <wilco@conduction.nl>
2020-10-22 14:01:39 +0200

    Created dashboard paging structure

---
+++ api/src/Controller/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+// src/Controller/ProcessController.php
+
+namespace App\Controller;
+
+use Conduction\CommonGroundBundle\Service\ApplicationService;
+//use App\Service\RequestService;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use function GuzzleHttp\Promise\all;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description.
+ *
+ * Class DashboardController
+ *
+ * @Route("/dashboard")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/general")
+     * @Template
+     */
+    public function generalAction(Session $session, Request $request, CommonGroundService $commonGroundService, ApplicationService $applicationService, ParameterBagInterface $params, string $slug = 'home')
+    {
+        $variables = [];
+
+        return $variables;
+    }
+
+    /**
+     * @Route("/security")
+     * @Template
+     */
+    public function securityAction(Session $session, Request $request, CommonGroundService $commonGroundService, ApplicationService $applicationService, ParameterBagInterface $params, string $slug = 'home')
+    {
+        $variables = [];
+
+        return $variables;
+    }
+}
