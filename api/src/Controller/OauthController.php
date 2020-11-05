eaa8b865e76f4ffa6b2a2b819985f274b1ab8475
Wilco Louwerse <wilco@conduction.nl>
2020-11-05 17:29:53 +0100

    Small 'typo' fix

--- api/src/Controller/OauthController.php
+++ api/src/Controller/OauthController.php
@@ -46,19 +46,19 @@ class OauthController extends AbstractController
         // Als localhost dan prima -> dit us wel unsave want ondersteund ook subdomein of path localhost
         if ($redirectUrl && strpos($redirectUrl, 'localhost')) {
             // $redirectUrl is al oke dus we hoeven niks te doen
-        } elseif ($redirectUrl &&  str_replace('http://','https://', $redirectUrl) != str_replace('http://','https://', $application['authorizationUrl'])) {
+        } elseif ($redirectUrl &&  str_replace('http://','https://', $redirectUrl) != str_replace('http://','https://', $variables['application']['authorizationUrl'])) {
             // $redirectUrl
         }
         else{
-            $redirectUrl = $application['authorizationUrl'];
+            $redirectUrl = $variables['application']['authorizationUrl'];
         }
 
         if ($request->isMethod('POST') && $request->get('grantAccess')) {
 
             if (strpos($request->get('redirect_uri'), 'localhost')) {
                 $redirectUrl = $request->get('redirect_uri');
-            } elseif ($request->$this->get('redirect_uri') == $application['authorizationUrl']) {
-                $redirectUrl = $application['authorizationUrl'];
+            } elseif ($request->$this->get('redirect_uri') == $variables['application']['authorizationUrl']) {
+                $redirectUrl = $variables['application']['authorizationUrl'];
             }
 
             if ($request->get('grantAccess') == 'true') {
@@ -68,7 +68,7 @@ class OauthController extends AbstractController
                 }
                 $state = $request->get('state');
                 $authorization = [];
-                $authorization['application'] = '/applications/'.$application['id'];
+                $authorization['application'] = '/applications/'.$variables['application']['id'];
                 $authorization['scopes'] = $request->get('scopes');
                 $authorization['goal'] = ' ';
                 $authorization['userUrl'] = $commonGroundService->cleanUrl(['component' => 'uc', 'type' => 'users', 'id' => $user['id']]);
