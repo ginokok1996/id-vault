c90c30e23130fa4e3afdaf7e25d0f7726f0c7d20
Barry Brands <barrybrands02@hotmail.com>
2020-11-09 13:51:05 +0100

    Bugfix OauthController

--- api/src/Controller/OauthController.php
+++ api/src/Controller/OauthController.php
@@ -84,7 +84,7 @@ class OauthController extends AbstractController
 
             if (strpos($request->get('redirect_uri'), 'localhost')) {
                 $redirectUrl = $request->get('redirect_uri');
-            } elseif ($this->get('redirect_uri') == $variables['application']['authorizationUrl']) {
+            } elseif ($request->get('redirect_uri') == $variables['application']['authorizationUrl']) {
                 $redirectUrl = $variables['application']['authorizationUrl'];
             }
 
