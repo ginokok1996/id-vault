2fb21a8648d8b590d2733de8fd7922c016ae82b5
ginokok1996 <ginokok1996@hotmail.com>
2021-01-04 16:42:07 +0100

    cleanup

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -29,10 +29,18 @@ class UserController extends AbstractController
      */
     private $flash;
     private $translator;
+    private $session;
+    private $request;
+    private $commonGroundService;
+    private $params;
 
-    public function __construct(FlashBagInterface $flash)
+    public function __construct(FlashBagInterface $flash, Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params)
     {
         $this->flash = $flash;
+        $this->session = $session;
+        $this->request = $request;
+        $this->commonGroundService = $commonGroundService;
+        $this->params = $params;
     }
 
     /**
@@ -40,23 +48,9 @@ class UserController extends AbstractController
      * @Route("/login/{loggedOut}", name="loggedOut")
      * @Template
      */
-    public function login(
-        Session $session,
-        Request $request,
-        AuthorizationCheckerInterface $authChecker,
-        CommonGroundService $commonGroundService,
-        ParameterBagInterface $params,
-        EventDispatcherInterface $dispatcher,
-        $loggedOut = false
-    ) {
-        $application = $commonGroundService->getResource(['component' => 'wrc', 'type' => 'applications', 'id' => $params->get('app_id')]);
+    public function login() {
+        $application = $this->commonGroundService->getResource(['component' => 'wrc', 'type' => 'applications', 'id' => $this->params->get('app_id')]);
 
-        if ($loggedOut == 'loggedOut') {
-            $text = 'U bent uitgelogd omdat de sessie is verlopen.';
-            $this->flash->add('error', $text);
-
-            $session->set('loggedOut', null);
-        }
         if ($this->getUser()) {
             $this->flash->add('success', 'Welcome '.ucwords($this->getUser()->getName()));
 
@@ -64,115 +58,27 @@ class UserController extends AbstractController
         }
 
         // Dealing with backUrls
-        if ($backUrl = $request->query->get('backUrl')) {
+        if ($backUrl = $this->request->query->get('backUrl')) {
         } else {
             $backUrl = '/dashboard';
         }
-        $session->set('backUrl', $backUrl);
+        $this->session->set('backUrl', $backUrl);
 
         return $this->redirect($this->generateUrl('app_default_index'));
-    }
-
-    /**
-     * @Route("/auth/digispoof")
-     * @Template
-     */
-    public function DigispoofAction(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
-    {
-        $redirect = $commonGroundService->cleanUrl(['component' => 'ds']);
-
-        return $this->redirect($redirect.'?responceUrl='.$request->query->get('response').'&backUrl='.$request->query->get('back_url'));
-    }
-
-    /**
-     * @Route("/auth/eherkenning")
-     * @Template
-     */
-    public function EherkenningAction(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
-    {
-        $redirect = $commonGroundService->cleanUrl(['component' => 'eh']);
-
-        return $this->redirect($redirect.'?responceUrl='.$request->query->get('response').'&backUrl='.$request->query->get('back_url'));
-    }
-
-    /**
-     * @Route("/auth/idin/login")
-     * @Template
-     */
-    public function IdinLoginAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
-    {
-        $session->set('backUrl', $request->query->get('backUrl'));
-
-        $redirect = str_replace('http:', 'https:', $request->getUri());
-        if (strpos($redirect, '?') == true) {
-            $redirect = substr($redirect, 0, strpos($redirect, '?'));
-        }
-
-        $provider = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'idin', 'application' => $params->get('app_id')])['hydra:member'];
-        $provider = $provider[0];
-
-        if (isset($provider['configuration']['app_id']) && isset($provider['configuration']['secret']) && isset($provider['configuration']['endpoint'])) {
-            $clientId = $provider['configuration']['app_id'];
-
-            if ($params->get('app_env') == 'prod') {
-                return $this->redirect('https://eu01.signicat.com/oidc/authorize?response_type=code&scope=openid+signicat.idin&client_id='.$clientId.'&redirect_uri='.$redirect.'&acr_values=urn:signicat:oidc:method:idin-login&state=123');
-            } else {
-                return $this->redirect('https://eu01.preprod.signicat.com/oidc/authorize?response_type=code&scope=openid+signicat.idin&client_id='.$clientId.'&redirect_uri='.$redirect.'&acr_values=urn:signicat:oidc:method:idin-login&state=123');
-            }
-        } else {
-            return $this->render('500.html.twig');
-        }
-    }
-
-    /**
-     * @Route("/auth/idin/ident")
-     * @Template
-     */
-    public function IdinAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
-    {
-        $session->set('backUrl', $request->query->get('backUrl'));
-
-        $redirect = str_replace('http:', 'https:', $request->getUri());
-        if (strpos($redirect, '?') == true) {
-            $redirect = substr($redirect, 0, strpos($redirect, '?'));
-        }
-
-        $provider = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'idin', 'application' => $params->get('app_id')])['hydra:member'];
-        $provider = $provider[0];
-
-        if (isset($provider['configuration']['app_id']) && isset($provider['configuration']['secret']) && isset($provider['configuration']['endpoint'])) {
-            $clientId = $provider['configuration']['app_id'];
-
-            if ($params->get('app_env') == 'prod') {
-                return $this->redirect('https://eu01.signicat.com/oidc/authorize?response_type=code&scope=openid+signicat.idin&client_id='.$clientId.'&redirect_uri='.$redirect.'&acr_values=urn:signicat:oidc:method:idin-ident&state=123');
-            } else {
-                return $this->redirect('https://eu01.preprod.signicat.com/oidc/authorize?response_type=code&scope=openid+signicat.idin&client_id='.$clientId.'&redirect_uri='.$redirect.'&acr_values=urn:signicat:oidc:method:idin-ident&state=123');
-            }
-        } else {
-            return $this->render('500.html.twig');
-        }
-    }
-
-    /**
-     * @Route("/auth/irma")
-     * @Template
-     */
-    public function IrmaAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
-    {
     }
 
     /**
      * @Route("/auth/facebook")
      * @Template
      */
-    public function FacebookAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
+    public function FacebookAction()
     {
-        $session->set('backUrl', $request->query->get('backUrl'));
+        $this->session->set('backUrl', $this->request->query->get('backUrl'));
 
-        $provider = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'facebook', 'application' => $params->get('app_id')])['hydra:member'];
+        $provider = $this->commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'facebook', 'application' => $this->params->get('app_id')])['hydra:member'];
         $provider = $provider[0];
 
-        $redirect = $request->getUri();
+        $redirect = $this->request->getUri();
         if (strpos($redirect, '?') == true) {
             $redirect = substr($redirect, 0, strpos($redirect, '?'));
         }
@@ -188,21 +94,21 @@ class UserController extends AbstractController
      * @Route("/auth/github")
      * @Template
      */
-    public function githubAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
+    public function githubAction()
     {
-        $session->set('backUrl', $request->query->get('backUrl'));
+        $this->session->set('backUrl', $this->request->query->get('backUrl'));
 
-        $providers = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'github', 'application' => $params->get('app_id')])['hydra:member'];
+        $providers = $this->commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'github', 'application' => $this->params->get('app_id')])['hydra:member'];
         $provider = $providers[0];
 
-        $redirect = $request->getUri();
+        $redirect = $this->request->getUri();
 
         if (strpos($redirect, '?') == true) {
             $redirect = substr($redirect, 0, strpos($redirect, '?'));
         }
 
         if (isset($provider['configuration']['app_id']) && isset($provider['configuration']['secret'])) {
-            return $this->redirect('https://github.com/login/oauth/authorize?state='.$params->get('app_id').'&redirect_uri='.$redirect.'&client_id='.$provider['configuration']['app_id']);
+            return $this->redirect('https://github.com/login/oauth/authorize?state='.$this->params->get('app_id').'&redirect_uri='.$redirect.'&client_id='.$provider['configuration']['app_id']);
         } else {
             return $this->render('500.html.twig');
         }
@@ -212,14 +118,14 @@ class UserController extends AbstractController
      * @Route("/auth/gmail")
      * @Template
      */
-    public function gmailAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
+    public function gmailAction()
     {
-        $session->set('backUrl', $request->query->get('backUrl'));
+        $this->session->set('backUrl', $this->request->query->get('backUrl'));
 
-        $providers = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'gmail', 'application' => $params->get('app_id')])['hydra:member'];
+        $providers = $this->commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'gmail', 'application' => $this->params->get('app_id')])['hydra:member'];
         $provider = $providers[0];
 
-        $redirect = $request->getUri();
+        $redirect = $this->request->getUri();
 
         if (strpos($redirect, '?') == true) {
             $redirect = substr($redirect, 0, strpos($redirect, '?'));
@@ -236,23 +142,23 @@ class UserController extends AbstractController
      * @Route("/auth/linkedin")
      * @Template
      */
-    public function linkedinAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
+    public function linkedinAction()
     {
-        if ($request->query->get('error')) {
+        if ($this->request->query->get('error')) {
             $this->flash->add('warning', 'LinkedIn authorization has been cancelled');
-            if ($session->get('backUrl')) {
-                return $this->redirect($session->get('backUrl'));
+            if ($this->session->get('backUrl')) {
+                return $this->redirect($this->session->get('backUrl'));
             } else {
                 return $this->redirect($this->generateUrl('app_default_index'));
             }
         }
 
-        $session->set('backUrl', $request->query->get('backUrl'));
+        $this->session->set('backUrl', $this->request->query->get('backUrl'));
 
-        $providers = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'linkedIn', 'application' => $params->get('app_id')])['hydra:member'];
+        $providers = $this->commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'linkedIn', 'application' => $this->params->get('app_id')])['hydra:member'];
         $provider = $providers[0];
 
-        $redirect = $request->getUri();
+        $redirect = $this->request->getUri();
 
         if (strpos($redirect, '?') == true) {
             $redirect = substr($redirect, 0, strpos($redirect, '?'));
@@ -269,7 +175,7 @@ class UserController extends AbstractController
      * @Route("/logout")
      * @Template
      */
-    public function logoutAction(Session $session, Request $request)
+    public function logoutAction()
     {
         return $this->redirect($this->generateUrl('app_default_login'));
     }
@@ -278,7 +184,7 @@ class UserController extends AbstractController
      * @Route("/register")
      * @Template
      */
-    public function registerAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params)
+    public function registerAction(Request $request, CommonGroundService $commonGroundService)
     {
         if ($request->isMethod('POST')) {
             $backUrl = $request->query->get('backUrl');
@@ -351,104 +257,5 @@ class UserController extends AbstractController
                 return $this->redirect($backUrl);
             }
         }
-    }
-
-    /**
-     * @Route("/userinfo")
-     * @Template
-     */
-    public function userInfoAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params)
-    {
-        $variables = [];
-
-        $variables['person'] = $commonGroundService->getResource($this->getUser()->getPerson());
-
-        if ($request->isMethod('POST') && $request->get('info')) {
-            $resource = $request->request->all();
-            $person = [];
-            $person['@id'] = $commonGroundService->cleanUrl(['component' => 'cc', 'type' => 'people', 'id' => $variables['person']['id']]);
-            $person['id'] = $variables['person']['id'];
-
-            if (isset($resource['firstName'])) {
-                $person['givenName'] = $resource['firstName'];
-            }
-            if (isset($resource['lastName'])) {
-                $person['familyName'] = $resource['lastName'];
-            }
-            if (isset($resource['birthday']) && $resource['birthday'] !== '') {
-                $person['birthday'] = $resource['birthday'];
-            }
-            if (isset($resource['email'])) {
-                $person['emails'][0]['email'] = $resource['email'];
-            }
-            if (isset($resource['telephone'])) {
-                $person['telephones'][0]['telephone'] = $resource['telephone'];
-            }
-            if (isset($resource['street'])) {
-                $person['adresses'][0]['street'] = $resource['street'];
-            }
-            if (isset($resource['houseNumber'])) {
-                $person['adresses'][0]['houseNumber'] = $resource['houseNumber'];
-            }
-            if (isset($resource['houseNumberSuffix'])) {
-                $person['adresses'][0]['houseNumberSuffix'] = $resource['houseNumberSuffix'];
-            }
-            if (isset($resource['postalCode'])) {
-                $person['adresses'][0]['postalCode'] = $resource['postalCode'];
-            }
-            if (isset($resource['locality'])) {
-                $person['adresses'][0]['locality'] = $resource['locality'];
-            }
-
-            $variables['person'] = $commonGroundService->saveResource($person, ['component' => 'cc', 'type' => 'people']);
-        } elseif ($request->isMethod('POST') && $request->get('password')) {
-            $newPassword = $request->get('newPassword');
-            $repeatPassword = $request->get('repeatPassword');
-
-            if ($newPassword !== $repeatPassword) {
-                $variables['error'] = true;
-
-                return $variables;
-            } else {
-                $credentials = [
-                    'username'   => $this->getUser()->getUsername(),
-                    'password'   => $request->request->get('currentPassword'),
-                    'csrf_token' => $request->request->get('_csrf_token'),
-                ];
-
-                $user = $commonGroundService->createResource($credentials, ['component'=>'uc', 'type'=>'login'], false, true, false, false);
-
-                if (!$user) {
-                    $variables['wrongPassword'] = true;
-
-                    return $variables;
-                }
-
-                $users = $commonGroundService->getResourceList(['component'=>'uc', 'type'=>'users'], ['username'=> $this->getUser()->getUsername()], true, false, true, false, false)['hydra:member'];
-                $user = $users[0];
-
-                $user['password'] = $newPassword;
-
-                $this->addFlash('success', 'wachtwoord aangepast');
-                $commonGroundService->updateResource($user);
-
-                $message = [];
-
-                if ($params->get('app_env') == 'prod') {
-                    $message['service'] = '/services/eb7ffa01-4803-44ce-91dc-d4e3da7917da';
-                } else {
-                    $message['service'] = '/services/1541d15b-7de3-4a1a-a437-80079e4a14e0';
-                }
-                $message['status'] = 'queued';
-                $message['data'] = ['receiver' => $variables['person']['name']];
-                $message['content'] = $commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'templates', 'id'=>'4125221c-74e0-46f9-97c9-3825a2011012']);
-                $message['reciever'] = $user['username'];
-                $message['sender'] = 'no-reply@conduction.nl';
-
-                $commonGroundService->createResource($message, ['component'=>'bs', 'type'=>'messages']);
-            }
-        }
-
-        return $variables;
     }
 }
